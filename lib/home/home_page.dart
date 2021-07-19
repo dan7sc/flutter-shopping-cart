0266451bd8fd0521603b4cbd0ca914bc6bed4649
dan7sc <su7cheng@gmail.com>
2021-07-19 15:04:04 -0300

    Show list of products

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,6 +1,8 @@
+import 'package:class_shopping_cart/home/home_controller.dart';
 import 'package:class_shopping_cart/modules/cart/cart_page.dart';
+import 'package:class_shopping_cart/shared/models/product_model.dart';
+import 'package:class_shopping_cart/state_builder.dart';
 import 'package:flutter/material.dart';
-
 
 class HomePage extends StatefulWidget {
   HomePage({Key? key}) : super(key: key);
@@ -10,19 +12,44 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final controller = HomeController();
+
+  @override
+  void initState() {
+    controller.getProducts();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         actions: [
-          IconButton(onPressed: () {
+          IconButton(
+            onPressed: () {
               Navigator.push(
-                context,
-                MaterialPageRoute(builder: (_) => CartPage()));
+                  context, MaterialPageRoute(builder: (_) => CartPage()));
             },
             icon: Icon(Icons.shopping_cart),
           ),
         ],
+      ),
+      body: StateBuilder<List<ProductModel>>(
+        controller: controller,
+        builder: (_, state) {
+          if (state.isEmpty) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return ListView.builder(
+            itemCount: state.length,
+            itemBuilder: (_, index) => ListTile(
+              title: Text(state[index].name),
+              trailing: Text(state[index].price.toString()),
+            ),
+          );
+        },
       ),
     );
   }
